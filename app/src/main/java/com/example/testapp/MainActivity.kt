5f431bf26b8a28b4f07cef7ba1dce8b5e775a750
reyyuvraj <yuvrajsingh77733999@gmail.com>
2023-02-18 16:47:02 +0530

    update files

--- app/src/main/java/com/example/testapp/MainActivity.kt
+++ app/src/main/java/com/example/testapp/MainActivity.kt
@@ -1,44 +1,12 @@
 package com.example.testapp
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import com.example.utils.showToast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val ntButton = findViewById<Button>(R.id.nt)
-        val toastButton = findViewById<Button>(R.id.toast)
-        val nhButton = findViewById<Button>(R.id.nh)
-
-        //to view the Notification Log
-        //open another app's activity from an action
-        ntButton.setOnClickListener {
-            val intent = Intent(Intent.ACTION_MAIN)
-            intent.setClassName(
-                "com.android.settings",
-                "com.android.settings.Settings\$NotificationStationActivity"
-            )
-            startActivity(intent)
-            finish()
-        }
-
-        toastButton.setOnClickListener {
-            showToast(this, "Right from utils module")
-        }
-
-        nhButton.setOnClickListener {
-            val intent = Intent(Intent.ACTION_MAIN)
-            intent.setClassName(
-                "com.android.settings",
-                "com.android.settings.notification.history.NotificationHistoryActivity"
-            )
-            startActivity(intent)
-            finish()
-        }
     }
 }
